8fdf0022ee3ecf55eefffa422dc1b8a183fd4423
aaron <aarontaylor@gmail.com>
2019-03-29 21:38:20 -0400

    add foreach to display data for both search options

--- src/doctor.js
+++ src/doctor.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
 
   $('#doctorName').click(function() {
     let search = $('#search').val();
-    $('#search').val("");
-
+    $('#search').val('');
+    $('.showName').text('');
     let doctor = new Doctor();  
     let promise = doctor.searchName(search); 
 
@@ -14,9 +14,57 @@ $(document).ready(function() {
 
       let body = JSON.parse(response);
 
-      $('.showName').append(`${body.data[0].profile.first_name}`);
+      let patient = (p) => (p == true) ? 'yes' : 'no';
+    
+      if (body.meta.count == 0) {
+      $('.showName').append(`Sorry no doctors found!`);
+      } else {
+
+        body.data.forEach(dr => {
+      
+          $('.showName').append(`
+          ${dr.profile.first_name} ${dr.profile.last_name}<br>
+          ${dr.practices[0].visit_address.street} ${dr.practices[0].visit_address.zip}<br>Phone: ${dr.practices[0].phones[0].number}<br>Accepting patients: ${patient(dr.practices[0].accepts_new_patients)}<br><br>`);
+
+        });
+          
+      }
+
       }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
+
+  $('#doctorSearch').click(function() {
+    let search = $('#search').val();
+    $('#search').val('');
+    $('.showName').text('');
+    let doctor = new Doctor();  
+    let promise = doctor.search(search); 
+
+    promise.then(function(response) {
+
+      let body = JSON.parse(response);
+
+      let patient = (p) => (p == true) ? 'yes' : 'no';
+      
+      if (body.meta.count == 0) {
+        $('.showName').append(`Sorry no doctors found!`);
+        } else {
+
+        body.data.forEach(dr => {
+      
+          $('.showName').append(`
+          ${dr.profile.first_name} ${dr.profile.last_name}<br>
+          ${dr.practices[0].visit_address.street} ${dr.practices[0].visit_address.zip}<br>Phone: ${dr.practices[0].phones[0].number}<br>Accepting patients: ${patient(dr.practices[0].accepts_new_patients)}<br><br>`);
+
+        });
+      
+      }
+
+      }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+
     });
   });
 
--- src/service.js
+++ src/service.js
@@ -1,4 +1,24 @@
 export default class Doctor {
+
+  search (search) {
+    return new Promise(function(resolve, reject) {
+
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2018-03-01/doctors?location=wa-seattle&query=${search}&user_key=${process.env.exports.apiKey}`;
+      
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+
+      request.open("GET", url, true);
+      request.send();
+
+    });
+  }
   searchName (name) {
     return new Promise(function(resolve, reject) {
 
